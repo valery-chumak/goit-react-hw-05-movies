420e1725ed30833272517a6fdf83395c80280060
valery-chumak <blossssom88@gmail.com>
2022-10-15 19:05:52 +0300

    Update UserRoute.jsx

--- src/components/UserRoute.jsx
+++ src/components/UserRoute.jsx
@@ -1,27 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
-// import { lazy, Suspense } from 'react';
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies/Movies';
-import MovieDetails from 'pages/MovieDetails/MovieDetails';
-import Reviews from 'pages/Reviews/Reviews';
-import Cast from 'pages/Cast/Cast';
-// const Home = lazy(() => import('pages/Home/Home'));
-// const Movies = lazy(() => import('pages/Movies/Movies'));
-// const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
-// const Reviews = lazy(() => import('pages/Reviews/Reviews'));
-// const Cast = lazy(() => import('pages/Cast/Cast'));
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Reviews = lazy(() => import('pages/Reviews/Reviews'));
+const Cast = lazy(() => import('pages/Cast/Cast'));
 export default function UserRoute() {
   return (
-    // <Suspense fallback={<p>Loading...</p>}>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Home />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/movies/:movieId" element={<MovieDetails />}>
-        <Route path="cast" element={<Cast />} />
-        <Route path="reviews" element={<Reviews />} />
-      </Route>
-    </Routes>
-    // </Suspense>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
